bea767b8baaf596c4b9c5472e7ba607f2213083a
ahb-ufz <anna.heintz-buschart@ufz.de>
2019-10-06 21:43:42 +0200

    taxonomy new, start ITSx

--- dada_scripts/dada_mergeRuns.R
+++ dada_scripts/dada_mergeRuns.R
@@ -44,18 +44,32 @@ if(length(seqTabList)>1){
 }
 if(snakemake@config[['chimeras']][['remove']]){
   print("Removing chimeras")
-  saveRDS(seqtab,snakemake@output[[4]])
+  saveRDS(seqtab,snakemake@output[[5]])
+  seqs1 <- DNAStringSet(colnames(seqtab))
+  seqtab1 <- seqtab
+#  names(seqs1) <- sprintf("OTU_%06d",1:length(seqs1))
+#  write.table(t(seqtab),snakemake@output[[6]],col.names=NA,sep="\t",quote=F)
+  seqtab <- removeBimeraDenovo(seqtab, method=snakemake@config[['chimeras']][['method']])
   seqs <- DNAStringSet(colnames(seqtab))
   names(seqs) <- sprintf("OTU_%06d",1:length(seqs))
-  writeXStringSet(seqs,snakemake@output[[5]])
-  write.table(t(seqtab),snakemake@output[[6]],col.names=NA,sep="\t",quote=F)
-  seqtab <- removeBimeraDenovo(seqtab, method=snakemake@config[['chimeras']][['method']])
+  seqs1set <- append(seqs,setdiff(seqs1,seqs))
+  names(seqs1set)[names(seqs1set)==""] <- sprintf("chimeric_OTU_%06d",(length(seqs)+1):length(seqs1set))
+  writeXStringSet(seqs1set,snakemake@output[[6]])
+  outtab1 <- merge(t(seqtab1),data.frame("OTU"=names(seqs1set),"seq"=seqs1set,stringsAsFactors=F),
+                   by.x=0,by.y="seq")
+  write.table(outtab1,snakemake@output[[7]],row.names=F,sep="\t",quote=F)
+}else{
+  seqs <- DNAStringSet(colnames(seqtab))
+  names(seqs) <- sprintf("OTU_%06d",1:length(seqs))
 }
 print("Saving merged OTU table")
 saveRDS(seqtab,snakemake@output[[1]])
-seqs <- DNAStringSet(colnames(seqtab))
-names(seqs) <- sprintf("OTU_%06d",1:length(seqs))
-writeXStringSet(seqs,snakemake@output[[2]])
-write.table(t(seqtab),snakemake@output[[3]],col.names=NA,sep="\t",quote=F)
+#seqs <- DNAStringSet(colnames(seqtab))
+#names(seqs) <- sprintf("OTU_%06d",1:length(seqs))
+writeXStringSet(seqs,snakemake@output[[3]])
+outtab <- merge(t(seqtab),data.frame("OTU"=names(seqs),"seq"=seqs,stringsAsFactors=F),
+                   by.x=0,by.y="seq")
+saveRDS(outtab,snakemake@output[[2]])
+write.table(outtab,snakemake@output[[4]],row.names=F,sep="\t",quote=F)
 
 
--- dada_scripts/decipher_ID.R
+++ dada_scripts/decipher_ID.R
@@ -20,13 +20,13 @@ library(DECIPHER)
 library(Biostrings)
 
 print("loading training set:")
-theoPath <- paste0(snakemake@config[['taxonomy']][['db_path']],"/",snakemake@config[['taxonomy']][['tax_db']])
+theoPath <- paste0(snakemake@config[['taxonomy']][['decipher']][['db_path']],"/",snakemake@config[['taxonomy']][['decipher']][['tax_db']])
 if(file.exists(theoPath)){
   loadPath <- theoPath
 }else{
-  altPath <- paste0(snakemake@config[['taxonomy']][['db_path']],"/",
-                    list.files(path=snakemake@config[['taxonomy']][['db_path']],
-                               pattern=snakemake@config[['taxonomy']][['tax_db']]))
+  altPath <- paste0(snakemake@config[['taxonomy']][['decipher']][['db_path']],"/",
+                    list.files(path=snakemake@config[['taxonomy']][['decipher']][['db_path']],
+                               pattern=snakemake@config[['taxonomy']][['decipher']][['tax_db']]))
   if(length(altPath)==1) loadPath <- altPath else stop(paste0(theoPath," not found."))
 }
 print(loadPath)
@@ -37,16 +37,14 @@ if(snakemake@config[['taxonomy']][['look_for_species']]){
   ranks <- c("domain","phylum","class","order","family","genus")
 }
 
-print(paste0("loading seqTab ",snakemake@input))
-seqtab <- readRDS(snakemake@input[[1]])
-seqs <- DNAStringSet(colnames(seqtab))
-names(seqs) <- sprintf("OTU_%06d",1:length(seqs))
+print(paste0("loading sequence ",snakemake@input))
+seqs <- readDNAStringSet(snakemake@input[[1]])
 
 print("running classification")
-set.seed(snakemake@config[['taxonomy']][['seed']])
-ids <- IdTaxa(seqs, trainingSet, strand=snakemake@config[['taxonomy']][['strand']],
-               threshold=snakemake@config[['taxonomy']][['threshold']],
-               bootstraps=snakemake@config[['taxonomy']][['bootstraps']], 
+set.seed(snakemake@config[['taxonomy']][['decipher']][['seed']])
+ids <- IdTaxa(seqs, trainingSet, strand=snakemake@config[['taxonomy']][['decipher']][['strand']],
+               threshold=snakemake@config[['taxonomy']][['decipher']][['threshold']],
+               bootstraps=snakemake@config[['taxonomy']][['decipher']][['bootstraps']], 
                processors=snakemake@threads, verbose=T)
 taxid <- t(sapply(ids, function(x) {
         m <- match(ranks, x$rank)
@@ -57,15 +55,15 @@ taxid <- t(sapply(ids, function(x) {
 colnames(taxid) <- ranks
 rownames(taxid) <- names(seqs)
 taxid <- taxid[order(rownames(taxid)),]
-otu <- t(seqtab)
-rownames(otu) <- names(seqs)
-otu <- otu[order(rownames(otu)),] 
-if(!all(rownames(taxid)==rownames(otu))) stop("mismatch between OTU names in taxonomy and OTU table")
+#otu <- t(seqtab)
+#rownames(otu) <- names(seqs)
+#otu <- otu[order(rownames(otu)),] 
+#if(!all(rownames(taxid)==rownames(otu))) stop("mismatch between OTU names in taxonomy and OTU table")
 
-write.table(data.frame(otu,"taxonomy"=apply(taxid,1,function(x) paste(x[!is.na(x)],sep=";",collapse=";")),
+write.table(data.frame("taxonomy"=apply(taxid,1,function(x) paste(x[!is.na(x)],sep=";",collapse=";")),
                        taxid,"seq"=as.character(seqs)[order(names(seqs))],stringsAsFactors=F),
             snakemake@output[[1]],sep="\t",col.names=NA,quote=F)
-saveRDS(data.frame(otu,"taxonomy"=apply(taxid,1,function(x) paste(x[!is.na(x)],sep=";",collapse=";")),
+saveRDS(data.frame("taxonomy"=apply(taxid,1,function(x) paste(x[!is.na(x)],sep=";",collapse=";")),
                        taxid,"seq"=as.character(seqs)[order(names(seqs))],stringsAsFactors=F),
         snakemake@output[[2]])
 
