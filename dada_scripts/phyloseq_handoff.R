1a933bc0c80550b896edb6657e920caab7fbeb26
ahb-ufz <anna.heintz-buschart@ufz.de>
2019-10-16 18:23:01 +0200

    hand-off to phyloseq R

---
+++ dada_scripts/phyloseq_handoff.R
@@ -0,0 +1,48 @@
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+.libPaths(paste0(snakemake@config[['dada_lib']],"/R/library"))
+
+library(BiocParallel)
+#parallel <- FALSE
+if (snakemake@threads > 1) {
+    library("BiocParallel")
+    # setup parallelization
+    register(MulticoreParam(snakemake@threads))
+    parallel <- TRUE
+}else{
+    parallel <- FALSE
+    register(SerialParam())
+}
+#register(SerialParam())
+library(phyloseq)
+library(Biostrings)
+
+# File parsing
+seqTab <- readRDS(snakemake@input[[1]])
+sInfo <- read.delim(snakemake@input[[2]],stringsAsFactors=F,row.names=1)
+
+if(snakemake@params[["currentStep"]]=="taxonomy"){
+ seqMat <- seqTab[,colnames(seqTab) %in% rownames(sInfo)]
+ rownames(seqMat) <- seqTab$Row.names
+ taxMat <- as.matrix(seqTab[,!colnames(seqTab) %in% c(rownames(sInfo),"Row.names")])
+ row.names(taxMat) <- seqTab$Row.names
+
+ seqPhy <- phyloseq(otu_table(seqMat,taxa_are_rows = T),
+                sample_data(sInfo),
+                tax_table(as.matrix(taxMat)))
+
+ seqs <- Biostrings::DNAStringSet(seqTab$Row.names)
+ names(seqs) <- seqTab$Row.names
+ seqPhy <- merge_phyloseq(seqPhy, seqs)
+ taxa_names(seqPhy) <- tax_table(seqPhy)[,"OTU"]
+
+}else if(snakemake@params[["currentStep"]]=="post"&snakemake@config[['postprocessing']][['treeing']]){
+ seqMat <- seqTab[,colnames(seqTab) %in% rownames(sInfo)]
+ rownames(seqMat) <- seqTab$Row.names
+ seqMeta <- seqTab[,!colnames(seqTab) %in% c(rownames(sInfo),"Row.names")]
+}
+writeRDS(seqPhy,snakemake@output[[1]])
+
+
