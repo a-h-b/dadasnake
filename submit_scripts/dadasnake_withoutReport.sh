ce3e80d9ce30dfb087c0aa0f4e43bd1cf18e40a7
Anna Heintz-Buschart <44941240+a-h-b@users.noreply.github.com>
2020-05-22 12:54:00 +0200

    threads for all submit version

--- submit_scripts/dadasnake_withoutReport.sh
+++ submit_scripts/dadasnake_withoutReport.sh
@@ -4,6 +4,7 @@ CONFIGFILE=$1
 VARCONFIG=$2
 DIR=$(dirname $VARCONFIG)
 JNAME=$3
+THREADS=$4
 
 while read var val; do unset $var ; declare $var="$val" ; done < $VARCONFIG
 if [ "$SNAKEMAKE_VIA_CONDA" = true ]; then
@@ -17,6 +18,6 @@ fi
 eval $LOADING_MODULES
 eval $CONDA_START
 
-snakemake -j 50 -s $DIR/Snakefile --cluster-config $DIR/config/$SCHEDULER.config.yaml --cluster "{cluster.call} {cluster.runtime}{params.runtime} {cluster.mem_per_cpu} {cluster.threads}{threads} {cluster.partition}" --configfile $CONFIGFILE --config sessionName=$JNAME --use-conda --conda-prefix $DIR/conda >> $JNAME.stdout 2>> $JNAME.stderr
+snakemake -j $THREADS -s $DIR/Snakefile --cluster-config $DIR/config/$SCHEDULER.config.yaml --cluster "{cluster.call} {cluster.runtime}{params.runtime} {cluster.mem_per_cpu} {cluster.threads}{threads} {cluster.partition}" --configfile $CONFIGFILE --config sessionName=$JNAME --use-conda --conda-prefix $DIR/conda >> $JNAME.stdout 2>> $JNAME.stderr
 
 eval $CONDA_END
