dad5de2159f9c18e28cdcb46048845085d000437
Anna Heintz-Buschart <44941240+a-h-b@users.noreply.github.com>
2020-10-26 14:29:12 +0100

    fix mothur NAs

--- workflow/scripts/prepare_blastn.R
+++ workflow/scripts/prepare_blastn.R
@@ -18,7 +18,7 @@ if(snakemake@config[['taxonomy']][['decipher']][['do']] &  snakemake@config[['ta
   names(missingTax) <- seqTab$OTU[is.na(seqTab$Domain.decipher) & is.na(seqTab$Kingdom.dada)]
 }else if(!snakemake@config[['taxonomy']][['decipher']][['do']] &  snakemake@config[['taxonomy']][['mothur']][['do']]){
   missingTax <- DNAStringSet(seqTab$Row.names[ seqTab$Domain.mothur ==""|is.na(seqTab$Domain.mothur) ])
-  names(missingTax) <- seqTab$OTU[seqTab$Domain.mothur ==""]
+  names(missingTax) <- seqTab$OTU[seqTab$Domain.mothur =="" | is.na(seqTab$Domain.mothur)]
 }else if(!snakemake@config[['taxonomy']][['decipher']][['do']] &  snakemake@config[['taxonomy']][['dada']][['do']]){
   missingTax <- DNAStringSet(seqTab$Row.names[is.na(seqTab$Kingdom.dada)])
   names(missingTax) <- seqTab$OTU[is.na(seqTab$Kingdom.dada)]
