db9f5dea1fd430d94f5c28ea242370cf1ac2caed
ahb-ufz <anna.heintz-buschart@ufz.de>
2021-02-02 10:49:59 +0100

    fix add_fungalTraits.R

--- workflow/scripts/add_fungalTraits.R
+++ workflow/scripts/add_fungalTraits.R
@@ -7,6 +7,7 @@ condap <- Sys.getenv("CONDA_PREFIX")
 
 print("reading OTU table")
 seqTab <- readRDS(snakemake@input[[1]])
+ori_colnames <- colnames(seqTab)
 
 print("reading FungalTraits table")
 fun <- read.delim(snakemake@config[['postprocessing']][['fungalTraits']][['db']])
@@ -16,7 +17,8 @@ seqTab <- merge(seqTab,fun,
                 by.x=paste0("Genus.",
                             snakemake@config[['postprocessing']][['fungalTraits']][['classifier']]),
                 by.y="GENUS.FungalTraits",all.x=T,sort=F)
-seqTab <- seqTab[,c(colnames(seqTab),setdiff(colnames(fun),colnames(seqTab)))]
+seqTab <- seqTab[,c(ori_colnames,setdiff(colnames(seqTab),ori_colnames))]
+
 
 print("Saving OTU table with FungalTraits")
 saveRDS(seqTab,snakemake@output[[2]])
